7349a0bc9770e0dda0c81e89c5c93412d2070f63
nikolai <mail@nikolai.fyi>
2022-02-26 09:29:36 -0400

    we value consistency in naming and correctness in documentation

--- sol/dmap.sol
+++ sol/dmap.sol
@@ -7,7 +7,7 @@ pragma solidity 0.8.10;
 
 contract Dmap {
     // storage: hash(zone, key) -> (val, flags)
-    // flags: locked (2^2) & dir (2^1)
+    // flags: locked (2^1) & dir (2^0)
     // log4: zone, key, val, flags
     // err: "LOCK"
 
@@ -19,9 +19,9 @@ contract Dmap {
     }
 
     function raw(bytes32 slot) payable external
-      returns (bytes32 val, bytes32 flags) {
+      returns (bytes32 value, bytes32 flags) {
         assembly {
-            val := sload(slot)
+            value := sload(slot)
             flags := sload(add(slot, 1))
         }
     }
@@ -29,22 +29,21 @@ contract Dmap {
     function get(address zone, bytes32 key) payable external
       returns (bytes32 value, bytes flags) {
         bytes32 hkey = keccak256(abi.encode(zone, key));
-        bytes32 flags;
         assembly {
             value := sload(hkey)
             flags := sload(add(hkey, 1))
         }
     }
 
-    function set(bytes32 key, bytes32 val, bytes32 flags) payable external {
+    function set(bytes32 key, bytes32 value, bytes32 flags) payable external {
         bytes32 hkey = keccak256(abi.encode(caller(), key));
         bytes32 prior;
         assembly {
             prior := sload(add(hkey, 1))
             if eq(2, and(prior, 2)) { revert("LOCK", 4) }
-            sstore(hkey, val)
+            sstore(hkey, value)
             sstore(add(hkey, 1), flags)
-            log4(caller(), key, val, flags, 0, 0)
+            log4(caller(), key, value, flags, 0, 0)
         }
     }
 }
