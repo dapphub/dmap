15ecf5ddcbb711465c17bbb492f3133fd27e0c90
nikolai <mail@nikolai.fyi>
2022-02-28 07:30:32 -0400

    simpler root zone

--- sol/root.sol
+++ sol/root.sol
@@ -7,8 +7,10 @@ pragma solidity 0.8.10;
 import { Dmap } from './dmap.sol';
 
 contract DmapRootZone {
-    Dmap                        public immutable dmap;
-    mapping(bytes32 => uint256) public           marks;
+    Dmap    public immutable dmap;
+    uint256 public           last;
+    bytes32 public           mark;
+    uint256 public immutable freq = 31 hours;
 
     event Mark(bytes32 indexed hash);
     event Etch(bytes32 indexed name, address indexed zone);
@@ -23,17 +25,18 @@ contract DmapRootZone {
     }
 
     function mark(bytes32 hash) external payable {
-        if (block.timestamp < marks[hash] + 6 hours) revert ErrPending();
-        marks[hash] = block.timestamp;
+        if (block.timestamp < last + freq) revert ErrPending();
         if (msg.value != 1 ether) revert ErrPayment();
         (bool ok, ) = block.coinbase.call{value:1 ether}("");
         if (!ok) revert ErrReceipt();
+        last = block.timestamp;
+        mark = hash;
         emit Mark(hash);
     }
 
     function etch(bytes32 salt, bytes32 name, address zone) external {
         bytes32 hash = keccak256(abi.encode(msg.sender, salt, name, zone));
-        if (block.timestamp > marks[hash] + 6 hours) revert ErrExpired();
+        if (block.timestamp >= last + freq) revert ErrExpired();
         dmap.set(name, bytes32(bytes20(zone)), true, true);
         emit Etch(name, zone);
     }
