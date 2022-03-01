9d0169ea1c7a2fdd74c03ef62cc7b0ed48786486
stobiewan <mark.stobie@gmail.com>
2022-03-02 01:20:26 +1300

    basic deploy and tests passing (#2)
    Co-authored-by: stobiewan <getsforwarded@gmail.com>

--- test/dmap-test.js
+++ test/dmap-test.js
@@ -6,14 +6,14 @@ const { send, want, snapshot, revert, b32 } = require('minihat')
 describe('dmap', ()=>{
     let dmap
     let rootzone
-//    let freezone
+    let freezone
 
     before(async ()=>{
         await hh.run('deploy-mock-dmap')
         const dapp = await dpack.load(require('../pack/dmap_full_hardhat.dpack.json'), hh.ethers)
         dmap = dapp.dmap
         rootzone = dapp.rootzone
-//        freezone = dapp.freezone
+        freezone = dapp.freezone
         await snapshot(hh)
     })
     beforeEach(async ()=>{
@@ -22,15 +22,16 @@ describe('dmap', ()=>{
 
     it('deploy postconditions', async ()=>{
         const [root_value, root_flags] = await dmap.raw('0x'+'0'.repeat(64))
-        want(root_value).eq(rootzone.address)
-        const dmap_ref = await root.dmap()
+        const slice_start = (256 - 160) / 4 + 2
+        want('0x'.concat(root_value.slice(slice_start))).eq(rootzone.address.toLowerCase())
+        const dmap_ref = await rootzone.dmap()
         want(dmap_ref).eq(dmap.address)
     })
 
     it('direct traverse', async ()=>{
         const root_free_slot = await dmap.slot(rootzone.address, b32('free'))
         const [root_free_value, root_free_flags] = await dmap.raw(root_free_slot)
-//        want(root_free_value).eq(freezone.address)
+//        want(root_free_value).eq(freezone.address)  // for this bit, we'll want to register the 'free' zone in the 'root' zone as part of the deploy sequence
     })
 
     it('basic set', async () => {
