a112d066780b0333b8b777ae98eb2e238c418fc2
Kevin Bravo <kbravo@protonmail.com>
2022-03-04 13:55:35 -0500

    set - do hashing inside assembly block

--- sol/dmap.sol
+++ sol/dmap.sol
@@ -36,9 +36,14 @@ contract Dmap {
     }
 
     function set(bytes32 key, bytes32 value, bytes32 flags) external {
-        bytes32 slot = keccak256(abi.encode(msg.sender, key));
+        bytes32 slot;
         bytes32 prior;
+        bytes32 encoding0;
+        bytes32 encoding1;
         assembly {
+            mstore(160, caller())
+            mstore(192, key)
+            slot := keccak256(160, 64)
             prior := sload(add(slot, 1))
             if eq(1, and(prior, 1)) { revert("LOCK", 4) }
             sstore(slot, value)
