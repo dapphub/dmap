c9e9869c477166592f79acb2c5668032394f2607
Nikolai Mushegian <924397+nmushegian@users.noreply.github.com>
2022-03-09 14:54:48 -0400

    flip bits, flip words, rename (#24)
    * renames
    
    * reverse flag bits
    
    * refactor root zone / deploy task
    
    * refactor free zone
    
    * refactor error code
    
    * one last thing

--- sol/dmap.sol
+++ sol/dmap.sol
@@ -6,59 +6,61 @@
 pragma solidity 0.8.11;
 
 contract Dmap {
-    // storage: hash(zone, key) -> (value, flags)
+    // storage: hash(zone, name) -> (meta, data)
     // flags: locked (2^0) & appflags
     // log4: zone, key, value, flags
     // err: "LOCK"
 
+    bytes32 immutable FLAG_LOCK = bytes32(uint256(1 << 255));
+
     constructor(address rootzone) {
+        bytes32 LOCK = FLAG_LOCK;
         assembly {
             sstore(0, shl(96, rootzone))
-            sstore(1, 3) // locked & 2^1
+            sstore(1, LOCK)
         }
     }
 
     function raw(bytes32 slot) external view
-      returns (bytes32 value, bytes32 flags) {
+      returns (bytes32 meta, bytes32 data) {
         assembly {
-            flags := sload(add(slot, 1))
-            value := sload(slot)
+            meta := sload(add(slot, 1))
+            data := sload(slot)
         }
     }
 
-    function get(address zone, bytes32 key) external view
-      returns (bytes32 value, bytes32 flags) {
+    function get(address zone, bytes32 name) external view
+      returns (bytes32 meta, bytes32 data) {
         assembly {
             mstore(0, zone)
-            mstore(32, key)
+            mstore(32, name)
             let slot := keccak256(0, 64)
-            flags := sload(add(slot, 1))
-            value := sload(slot)
+            meta := sload(add(slot, 1))
+            data := sload(slot)
         }
     }
 
-    error LOCK();
-    bytes4 constant locksel = 0xa4f0d7d0; // keccak256("LOCK()")
-    function set(bytes32 key, bytes32 value, bytes32 flags) external {
+    function set(bytes32 name, bytes32 meta, bytes32 data) external {
+        bytes32 LOCK = FLAG_LOCK;
         assembly {
-            log4(0, 0, caller(), key, value, flags)
-            mstore(32, key)
+            log4(0, 0, caller(), name, data, meta)
+            mstore(32, name)
             mstore(0, caller())
             let slot0 := keccak256(0, 64)
             let slot1 := add(slot0, 1)
-            if and(1, sload(slot1)) {
-                mstore(0, locksel)
+            if and(LOCK, sload(slot1)) {
+                mstore(0, "LOCK")
                 revert(0, 4)
             }
-            sstore(slot0, value)
-            sstore(slot1, flags)
+            sstore(slot0, data)
+            sstore(slot1, meta)
         }
     }
 
-    function slot(address zone, bytes32 key) external pure returns (bytes32 slot) {
+    function slot(address zone, bytes32 name) external pure returns (bytes32 slot) {
         assembly {
             mstore(0, zone)
-            mstore(32, key)
+            mstore(32, name)
             slot := keccak256(0, 64)
         }
     }
