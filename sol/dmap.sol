dfa305ffba638018b7208bce449f9a2b9fef5f77
Nikolai Mushegian <924397+nmushegian@users.noreply.github.com>
2022-04-06 07:40:08 -0400

    slot (#62)
    * slot
    
    * pair
    
    * pair
    
    * gas costs

--- sol/dmap.sol
+++ sol/dmap.sol
@@ -44,4 +44,19 @@ contract Dmap {
         }
     }
 
+    function slot(bytes32 s) external view
+      returns (bytes32 a) {
+        assembly {
+            a := sload(s)
+        }
+    }
+
+    function pair(bytes32 s) external view
+      returns (bytes32 a, bytes32 b) {
+        assembly {
+            a := sload(s)
+            b := sload(add(s, 1))
+        }
+    }
+
 }
