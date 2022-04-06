640afeb62d1a1ef0e28f46a615133902c8a072ae
nikolai <mail@nikolai.fyi>
2022-04-05 22:06:18 -0400

    it compiles

--- sol/dmap2.sol
+++ sol/dmap2.sol
@@ -1,33 +1,34 @@
-{
-  let iu := calldataload(0x00)
-  let iv := add(1, iu)
 
-  // Reads take the key pre-hashed
-  if eq(calldatasize, 0x20) {
-    mstore(0x00, sload(iu))
-    mstore(0x20, sload(iv))
-    return(0, 0x40)
-  }
+contract Dmap2 { fallback() external { assembly {
+    let iu := calldataload(0x00)
+    let iv := add(1, iu)
 
-  let k := calldataload(0x20)
-  let u := calldataload(0x40)
-  let v := calldataload(0x60)
+    // Reads take the key pre-hashed
+    if eq(calldatasize(), 0x20) {
+      mstore(0x00, sload(iu))
+      mstore(0x20, sload(iv))
+      return(0, 0x40)
+    }
 
-  // Check key hash
-  mstore(0x00, caller)
-  mstore(0x14, k)
-  if not(eq(iu, keccak256(0, 0x34)) {
-    revert(0, 0)
-  }
+    let k := calldataload(0x20)
+    let u := calldataload(0x40)
+    let v := calldataload(0x60)
 
-  // Check lock flag
-  if and(1, sload(iu)) {
-    mstore(0, 0xa4f0d7d0)
-    revert(0, 4)
-  }
+    // Check key hash
+    mstore(0x00, caller())
+    mstore(0x14, k)
+    if not(eq(iu, keccak256(0, 0x34))) {
+      revert(0, 0)
+    }
 
-  sstore(iu, u)
-  sstore(iv, v)
+    // Check lock flag
+    if and(1, sload(iu)) {
+      mstore(0, 0xa4f0d7d0)
+      revert(0, 4)
+    }
 
-  log4(0, 0, caller, k, u, v)
-}
+    sstore(iu, u)
+    sstore(iv, v)
+
+    log4(0, 0, caller(), k, u, v)
+} } }
