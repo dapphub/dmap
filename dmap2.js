def356c546f3cedb986de89a96d9425baab03ef6
nikolai <mail@nikolai.fyi>
2022-04-27 18:11:02 -0400

    sketch dmap2

--- dmap2.js
+++ dmap2.js
@@ -1,18 +1,17 @@
-const dmap = {}
 
-const test =(s,f)=> {
-    if (process && process.argv.includes('--test')) {
-        require('tapzero').test(s,f)
-    }
+
+const pack = require('./pack/premap.dpack.json')
+const artifact = require('./pack/Dmap.json') // TODO verify (or load via CID)
+
+module.exports = dmap = {}
+
+dmap.address = pack.objects.dmap.address;
+dmap.abi = artifact.abi; // TODO check == pack.types.Dmap->artifact.abi
+
+let test = ()=>{}
+if (process && process.argv.includes('--test')) {
+    test = require('tapzero').test
 }
-
-dmap.address = ""
-dmap.abi = {}
-
-test('address', ()=>{
-    console.log('should only run when using `node dmap.js --test`')
-})
-
 
 dmap._getStorageSlot = async (slot)=> {
     fetch(ETH_RPC_URL, {method:"", params:[]})
@@ -33,4 +32,16 @@ dmap.getPair = async (zone,name)=> {
 dmap.getPairByHash = async (hash)=> {
 }
 
-module.exports = dmap
+test('meta', t=>{
+    t.ok(process.argv.includes('--test'),
+         `should only run when using 'node dmap.js --test`)
+})
+
+test('address', t=>{
+    t.equal(dmap.address.length, 42) // 0x + 20 bytes
+})
+
+test('abi', t=>{
+    t.ok(dmap.abi)
+})
+
