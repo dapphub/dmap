b88214a1a455320e2c796872f762e276e90c724c
stobiewan <getsforwarded@gmail.com>
2022-05-23 20:37:03 +1200

    use hear from minihat

--- test/utils/helpers.js
+++ test/utils/helpers.js
@@ -1,41 +1,7 @@
-// redo expectEvent to work with ethers
-//     https://github.com/OpenZeppelin/openzeppelin-test-helpers/blob/master/src/expectEvent.js
-//
-// The MIT License (MIT)
-// Copyright (c) 2018 OpenZeppelin
-// https://github.com/OpenZeppelin/openzeppelin-test-helpers/blob/master/LICENSE
-
 const { ethers } = require("hardhat");
-const {expect} = require("chai");
 const { want } = require('minihat')
 const {hexZeroPad} = require("@ethersproject/bytes");
 const lib = require('../../dmap')
-
-// matches eventName
-// matches data if defined
-function expectEvent (receipt, eventName, eventArgs = {}, data = '0x') {
-    const args = Object.keys(eventArgs).map((key) => {return eventArgs[key]})
-    let found = false
-    receipt.events.forEach(event => {
-        if( event.event == eventName && data == event.data ) {
-            let match = true
-            Object.keys(eventArgs).forEach(key => {
-                try {
-                    if( eventName == undefined ) {
-                        expect(eventArgs[key]).to.eql(event.topics[key])
-                    } else {
-                        expect(eventArgs[key]).to.eql(event.args[key])
-                    }
-                } catch {
-                    match = false
-                }
-            })
-            found = found || match
-        }
-    })
-
-    expect(found).to.equal(true, `No '${eventName}' events found with args ${args} and data ${data}`);
-}
 
 function padRight(addrStr) {
     const numBits = 256 - 160
@@ -87,4 +53,4 @@ const check_entry = async (dmap, usr, key, _meta, _data) => {
 }
 
 
-module.exports = { expectEvent, padRight, check_gas, check_entry, testlib }
+module.exports = { padRight, check_gas, check_entry, testlib }
